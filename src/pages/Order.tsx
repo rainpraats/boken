970011587cd3138b670f179924255464c86a1a44
rainpraats <rainpraats@gmail.com>
2025-04-25 11:09:25 +0200

    pressing submit order saves to database

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,17 +1,81 @@
+import { useState } from 'react';
 import { Link } from 'react-router';
 
+interface orderInterface {
+  careOf: string;
+  email: string;
+  familyName: string;
+  givenName: string;
+  locality: string;
+  paymentMethod: string;
+  phoneNumber: string;
+  postalCode: string;
+  streetAdress: string;
+  quantity: string;
+}
+
 const Order: React.FC = () => {
+  const [showSwishDetails, setShowSwishDetails] = useState(false);
+  const [showBankDetails, setShowBankDetails] = useState(false);
+
+  const handleSubmitOrder = async (e: any) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const orderData = Object.fromEntries(formData.entries());
+
+    const order: orderInterface = {
+      careOf: orderData.careOf.toString(),
+      email: orderData.email.toString(),
+      familyName: orderData.familyName.toString(),
+      givenName: orderData.givenName.toString(),
+      locality: orderData.locality.toString(),
+      paymentMethod: orderData.paymentMethod.toString(),
+      phoneNumber: orderData.phoneNumber.toString(),
+      postalCode: orderData.postalCode.toString(),
+      streetAdress: orderData.streetAdress.toString(),
+      quantity: orderData.quantity.toString(),
+    };
+
+    try {
+      const response = await fetch('http://localhost:3000/order', {
+        method: 'POST',
+        headers: {
+          'COntent-Type': 'application/json',
+        },
+        body: JSON.stringify(order),
+      });
+
+      if (response.ok) {
+        return true;
+      } else {
+        throw new Error(
+          `POST failed ${response.status} ${response.statusText}`
+        );
+      }
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  };
+
   return (
     <main>
       <Link className="return" to="/" title="Återvänd hem.">
         &lsaquo;&lsaquo; Tillbaka
       </Link>
-      <form>
+      <form onSubmit={handleSubmitOrder}>
         <div>
           <p>Bakom Skuggorna</p>
           <label>
             Antal:
-            <input type="number" min="0" defaultValue="1" max="35" />
+            <input
+              type="number"
+              name="quantity"
+              min="0"
+              defaultValue="1"
+              max="35"
+            />
           </label>
         </div>
         <label>
@@ -26,7 +90,7 @@ const Order: React.FC = () => {
         <label>
           <input
             type="tel"
-            name="phonenumber"
+            name="phoneNumber"
             placeholder="Mobiltelefonnummer"
             autoComplete="tel"
           />
@@ -34,7 +98,7 @@ const Order: React.FC = () => {
         <label>
           <input
             type="text"
-            name="given-name"
+            name="givenName"
             placeholder="Förnamn"
             autoComplete="given-name"
             required
@@ -43,7 +107,7 @@ const Order: React.FC = () => {
         <label>
           <input
             type="text"
-            name="family-name"
+            name="familyName"
             placeholder="Efternamn"
             autoComplete="family-name"
             required
@@ -52,7 +116,7 @@ const Order: React.FC = () => {
         <label>
           <input
             type="text"
-            name="care-of"
+            name="careOf"
             placeholder="c/o"
             autoComplete="name"
           />
@@ -60,7 +124,7 @@ const Order: React.FC = () => {
         <label>
           <input
             type="text"
-            name="postal-code"
+            name="postalCode"
             placeholder="Postnummer"
             autoComplete="home postal-code"
             required
@@ -78,7 +142,7 @@ const Order: React.FC = () => {
         <label>
           <input
             type="text"
-            name="street-address"
+            name="streetAdress"
             placeholder="Adress"
             autoComplete="home street-address"
             required
@@ -89,8 +153,12 @@ const Order: React.FC = () => {
           <input
             type="radio"
             id="swish"
-            name="payment_method"
+            name="paymentMethod"
             defaultValue="swish"
+            onChange={(e) => {
+              setShowSwishDetails(e.target.checked);
+              setShowBankDetails(false);
+            }}
             required
           />
         </label>
@@ -98,11 +166,31 @@ const Order: React.FC = () => {
           Banköverföring:
           <input
             type="radio"
-            id="bank_transfer"
-            name="payment_method"
-            defaultValue="Banköverföring"
+            name="paymentMethod"
+            defaultValue="banktransfer"
+            onChange={(e) => {
+              setShowBankDetails(e.target.checked);
+              setShowSwishDetails(false);
+            }}
           />
         </label>
+        {showSwishDetails && (
+          <p>
+            Vänligen swisha till följande telefonnummer: 070 1234-5678. Ange
+            ditt namn som referens.
+          </p>
+        )}
+        {showBankDetails && (
+          <p>
+            Vänligen gör en banköverföring till följande konto: 1234-5678. Ange
+            ditt namn som referens.
+          </p>
+        )}
+        {showSwishDetails || showBankDetails ? (
+          <button type="submit">Skicka beställning</button>
+        ) : (
+          <></>
+        )}
       </form>
     </main>
   );
